9f19f03d30cc8f3ca4f4bbfc17f2e9189ff0dcf9
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-09-25 13:36:19 -0400

    Add code skeleton

---
+++ EHR_Cleaning_Implementations/muthalagu_2014.R
@@ -0,0 +1,13 @@
+# Implementing Muthalagu, et al. (2014)
+# By Hannah De los Santos
+# Originated on: 9/25/2020
+
+# paper: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3974252/
+
+# load data and libraries ----
+
+# supporting functions ----
+
+# overarching data ----
+
+# this includes specified cutoffs, etc.
