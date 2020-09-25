2afa6e8b91a6eb87d5ad03e0520df95d1a93e913
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-09-25 13:55:39 -0400

    Add fake data

--- EHR_Cleaning_Implementations/muthalagu_2014.R
+++ EHR_Cleaning_Implementations/muthalagu_2014.R
@@ -6,6 +6,27 @@
 
 # load data and libraries ----
 
+# fake data, for ease of coding purposes
+set.seed(8)
+num_subj <- 5
+age_years <- seq(18, 68, by = 2.5)
+min_height <- 4*30.48 # 4 ft
+max_height <- 8*30.48 # 8 ft
+df <- data.frame(
+  "id" = 1:(length(age_years)*num_subj),
+  "subjid" = rep(1:num_subj, each = length(age_years)),
+  "sex" = rep(sample(c(0,1), num_subj, replace = T), each = length(age_years)),
+  "age_years" = rep(age_years, num_subj),
+  "param" = "HEIGHTCM",
+  "measurement" = rep(runif(num_subj, min = min_height, max = max_height),
+                      each = length(age_years))
+)
+# jitter each of the measurements for interest
+for (i in 1:num_subj){
+  df$measurement[df$subjid == i] <- 
+    df$measurement[df$subjid == i]+ rnorm(sum(df$subjid == i))
+}
+
 # supporting functions ----
 
 # overarching data ----
