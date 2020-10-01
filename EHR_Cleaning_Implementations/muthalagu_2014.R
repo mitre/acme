0a4bf1d90b5fbe140334a810e5e2d00f7ef50261
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-01 10:15:16 -0400

    Add steps 2b and 2c

--- EHR_Cleaning_Implementations/muthalagu_2014.R
+++ EHR_Cleaning_Implementations/muthalagu_2014.R
@@ -11,7 +11,7 @@
 # fake data, for ease of coding purposes
 set.seed(8)
 num_subj <- 5
-age_years <- seq(18, 68, by = 2.5)
+age_years <- seq(18, 68, by = .8)
 min_height <- 4*30.48 # 4 ft
 max_height <- 8*30.48 # 8 ft
 df <- data.frame(
@@ -26,7 +26,7 @@ df <- data.frame(
 # jitter each of the measurements for interest
 for (i in 1:num_subj){
   df$measurement[df$subjid == i] <- 
-    df$measurement[df$subjid == i]+ rnorm(sum(df$subjid == i))
+    df$measurement[df$subjid == i]+ rnorm(sum(df$subjid == i), 0, 1.5)
 }
 
 # supporting functions ----
@@ -54,7 +54,7 @@ age_ranges <- data.frame(
 # preallocate final designation
 out <- c()
 # go through each subject
-for (i in unique(subjid)){
+for (i in unique(df$subjid)){
   # since we're going to do this a fair bit
   slog <- df$subjid == i
   
@@ -76,6 +76,12 @@ for (i in unique(subjid)){
     subj_df_age <- subj_df[subj_df$age_years >= age_ranges$low[ab] &
                              subj_df$age_years < age_ranges$high[ab],]
     
+    # if there's nothing in this age range, there's nothing to consider
+    # NOTE: using next to avoid further nesting
+    if (nrow(subj_df_age) == 0){
+      next
+    }
+    
     if (abs(max(subj_df_age$measurement) - min(subj_df_age$measurement)) >= 
         btwn_range_cutoff){
       # 2b: if not in range, calculate median height at each age. compare with 
@@ -83,7 +89,71 @@ for (i in unique(subjid)){
       # and next median, flag as potentially erroneous
       # if only 2 valid medians and differ by >3.5, flag both as indeterminate
       
+      # if there are only two values, everything will be indeterminate
+      if (nrow(subj_df_age) <= 2){
+        subj_keep[as.character(subj_df_age$id)] <- "Indeterminate"
+      } else {
+        # calculate median heights per age (rounded to nearest year) in bucket
+        med_hts <- sapply(round(unique(subj_df_age$age_years)), function(x){
+          median(subj_df_age$measurement[round(subj_df_age$age_years) == x])
+        })
+        
+        # compare only for middle values -- end values are indeterminate
+        # results in true if both are under the cutoff
+        mid_compare <- sapply(2:(length(med_hts)-1), function(x){
+          !all(c(abs(med_hts[x] - med_hts[x-1]), abs(med_hts[x] - med_hts[x+1])) >= 
+            btwn_range_cutoff)
+        })
+        
+        # if true, correct height, else, erroneous
+        # also include indeterminate (first and last)
+        err_hts_idx <- c(1, 
+                         if(!all(mid_compare)){which(!mid_compare)+1}, 
+                         length(med_hts))
+        
+        # 2c: For erroneous and indeterminate medians, assign algorithms within
+        # 3 year period. Then compare all other recorded heights to the median at 
+        # that are. If the recorded height for any age differs  > 3.5 (for 
+        # erroneous) or > 6 (for indeterminate) from cleaned median height for 
+        # that age, the value is erroneous.
+        
+        # if there's no correct median height for comparison, it's all indeterminate
+        if (sum(mid_compare) == 0){
+          subj_keep[as.character(subj_df_age$id[err_hts_idx])] <- "Indeterminate"
+        } else {
+          chg_err_med <- sapply(err_hts_idx, function(x){
+            # get all the correct medians within a 3 year window
+            sub_df <- subj_df_age[-err_hts_idx,] # do not incorrect ages
+            avail_med <- 
+              med_hts[-err_hts_idx][sub_df$age_years <= (sub_df$age_years[x]+1.5) |
+                                      sub_df$age_years >= (sub_df$age_years[x]-1.5)]
+            
+            # assign nearest correct median
+            new_med <- avail_med[which.min(abs(avail_med - med_hts[x]))]
+            
+            # compare cleaned median to all recorded heights
+            compare <- abs(subj_df_age$measurement[-x]-new_med)
+            # for end points, they have a wider range
+            if (x == 1 | x == length(med_hts)){
+              if(any(compare > 6)){
+                return("Erroneous")
+              } else {
+                return("Include")
+              }
+            } else {
+              if(any(compare >= btwn_range_cutoff)){
+                return("Erroneous")
+              } else {
+                return("Include")
+              }
+            }
+            
+          })
+          
+          # adjust the errored/indeterminate heights accordingly
+          subj_keep[as.character(subj_df_age$id[err_hts_idx])] <- chg_err_med
+        }
+      }
     }
-    
   }
 }
