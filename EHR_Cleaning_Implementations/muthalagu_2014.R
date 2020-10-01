0a408e0082a1b7a95bb11c839f4d0d58b32023f2
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-01 11:56:40 -0400

    Remove data (to be moved to shiny app in future)

--- EHR_Cleaning_Implementations/muthalagu_2014.R
+++ EHR_Cleaning_Implementations/muthalagu_2014.R
@@ -6,29 +6,6 @@
 
 # Note: will have to update to data table upon completion for speed
 
-# load data ----
-
-# fake data, for ease of coding purposes
-set.seed(8)
-num_subj <- 5
-age_years <- seq(18, 68, by = .8)
-min_height <- 4*30.48 # 4 ft
-max_height <- 8*30.48 # 8 ft
-df <- data.frame(
-  "id" = 1:(length(age_years)*num_subj),
-  "subjid" = rep(1:num_subj, each = length(age_years)),
-  "sex" = rep(sample(c(0,1), num_subj, replace = T), each = length(age_years)),
-  "age_years" = rep(age_years, num_subj),
-  "param" = "HEIGHTCM",
-  "measurement" = rep(runif(num_subj, min = min_height, max = max_height),
-                      each = length(age_years))
-)
-# jitter each of the measurements for interest
-for (i in 1:num_subj){
-  df$measurement[df$subjid == i] <- 
-    df$measurement[df$subjid == i]+ rnorm(sum(df$subjid == i), 0, 2)
-}
-
 # implement muthalagu, et al. ----
 
 # function to clean height data by muthalagu, et al.
@@ -191,5 +168,3 @@ muthalagu_clean_ht <- function(df){
   
   return(df)
 }
-
-clean_df <- muthalagu_clean_ht(df)
