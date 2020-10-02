a80e7f82ec0407764a86f30860caae4b80bee004
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-02 16:19:15 -0400

    Functionalize for future shiny app

--- EHR_Cleaning_Implementations/cheng_2016.R
+++ EHR_Cleaning_Implementations/cheng_2016.R
@@ -54,122 +54,138 @@ remove_biv <- function(subj_df, type, biv_df){
 
 # implement cheng, et al. ----
 
-# method specific constants ----
-# this includes specified cutoffs, etc.
-
-# BIVs
-biv_df <- data.frame(
-  "low" = c(111.8,24.9),
-  "high" = c(228.6,453.6)
-)
-rownames(biv_df) <- c("height", "weight")
-
-# begin implementation ----
-
-# preallocate final designation
-df$result <- "Include"
-rownames(df) <- df$id
-# go through each subject
-for (i in unique(df$subjid)){
-  slog <- df$subjid == i
+# function to clean height and weight data by cheng, et al.
+# inputs:
+# df: data frame with 7 columns:
+#   id: row id
+#   subjid: subject id
+#   sex: sex of subject
+#   age_years: age, in years
+#   param: HEIGHTCM or WEIGHTKG
+#   measurement: height or weight measurement
+# outputs:
+#   df, with additional column "result", which specifies whether the height 
+#     measurement should be included or implausible.
+cheng_clean_both <- function(df){
+  # method specific constants ----
+  # this includes specified cutoffs, etc.
   
-  # start with height ----
+  # BIVs
+  biv_df <- data.frame(
+    "low" = c(111.8,24.9),
+    "high" = c(228.6,453.6)
+  )
+  rownames(biv_df) <- c("height", "weight")
   
-  h_df <- df[df$param == "HEIGHTCM" & slog,]
+  # begin implementation ----
   
-  subj_keep <- rep("Include", nrow(h_df))
-  names(subj_keep) <- h_df$id
-  
-  subj_df <- h_df
-  
-  # 1h ----
-  # 1h. remove biologically impossible height records
-  criteria <- remove_biv(subj_df, "height", biv_df)
-  subj_keep[criteria] <- "Implausible"
-  
-  subj_df <- subj_df[!criteria,]
-  
-  if (nrow(subj_df) > 0){
-    #2h ----
-    # 2h. Exclude height if a) absolute difference between that height and average
-    # height > standard deviation (SD) AND b) SD > 2.5% of average height.
+  # preallocate final designation
+  df$result <- "Include"
+  rownames(df) <- df$id
+  # go through each subject
+  for (i in unique(df$subjid)){
+    slog <- df$subjid == i
     
-    # calculate standard deviation of height for a given subject
-    st_dev <- sd(subj_df$measurement)
-    avg <- mean(subj_df$measurement)
+    # start with height ----
     
-    # if the standard deviation is > than 2.5%, we can say something about height
-    if (st_dev/avg > .025){
-      # criteria (a)
-      excl_ht <- sapply(subj_df$measurement, function(x){abs(x-avg) > st_dev})
+    h_df <- df[df$param == "HEIGHTCM" & slog,]
+    
+    subj_keep <- rep("Include", nrow(h_df))
+    names(subj_keep) <- h_df$id
+    
+    subj_df <- h_df
+    
+    # 1h ----
+    # 1h. remove biologically impossible height records
+    criteria <- remove_biv(subj_df, "height", biv_df)
+    subj_keep[criteria] <- "Implausible"
+    
+    subj_df <- subj_df[!criteria,]
+    
+    if (nrow(subj_df) > 0){
+      #2h ----
+      # 2h. Exclude height if a) absolute difference between that height and average
+      # height > standard deviation (SD) AND b) SD > 2.5% of average height.
       
-      subj_keep[as.character(subj_df$id[excl_ht])] <- "Implausible"
+      # calculate standard deviation of height for a given subject
+      st_dev <- sd(subj_df$measurement)
+      avg <- mean(subj_df$measurement)
+      
+      # if the standard deviation is > than 2.5%, we can say something about height
+      if (st_dev/avg > .025){
+        # criteria (a)
+        excl_ht <- sapply(subj_df$measurement, function(x){abs(x-avg) > st_dev})
+        
+        subj_keep[as.character(subj_df$id[excl_ht])] <- "Implausible"
+      }
     }
+    
+    h_df$result <- subj_keep
+    
+    # add results to full dataframe
+    df[as.character(h_df$id), "result"] <- h_df$result
+    
+    # then do weight ----
+    
+    w_df <- df[df$param == "WEIGHTKG" & slog,]
+    
+    subj_keep <- rep("Include", nrow(w_df))
+    names(subj_keep) <- w_df$id
+    
+    subj_df <- w_df
+    
+    # 1w ----
+    # 1w. remove biologically impossible weight records
+    criteria <- remove_biv(subj_df, "weight", biv_df)
+    subj_keep[criteria] <- "Implausible"
+    
+    subj_df <- subj_df[!criteria,]
+    
+    if (nrow(subj_df) > 0){
+      # 2w ----
+      # 2w. weight inaccurate if:
+      # a) the range was > 22.7 kg AND absolute difference between recorded weight 
+      # and avg weight was > 70% of range
+      # OR
+      # b) SD was >20% of the average weight AND absolute difference between that 
+      # weight and average weight > the SD
+      
+      avg_w <- mean(subj_df$measurement)
+      
+      # first calculate criteria a)
+      w_range <- abs(max(subj_df$measurement) - min(subj_df$measurement))
+      # if range is > 22.7, we can possibly say something about weight
+      criteria_a <- 
+        if (w_range > 22.7){
+          # weight difference is more than 70% of range
+          sapply(subj_df$measurement, function(x){
+            abs(x - avg_w) > .7*w_range
+          })
+        } else {
+          rep(F, nrow(subj_df))
+        }
+      
+      # criteria b)
+      st_dev <- sd(subj_df$measurement)
+      # if sd was >20% of avg weight, we can possibly say something about weight
+      criteria_b <- 
+        if (st_dev/avg_w > .2){
+          # weight difference is > SD
+          sapply(subj_df$measurement, function(x){
+            abs(x - avg_w) > st_dev
+          })
+        } else {
+          rep(F, nrow(subj_df))
+        }
+      
+      subj_keep[as.character(subj_df$id[criteria_a | criteria_b])] <- "Implausible"
+    }
+    
+    w_df$result <- subj_keep
+    
+    # add results to full dataframe
+    df[as.character(w_df$id), "result"] <- w_df$result
   }
   
-  h_df$result <- subj_keep
-  
-  # add results to full dataframe
-  df[as.character(h_df$id), "result"] <- h_df$result
-  
-  # then do weight ----
-  
-  w_df <- df[df$param == "WEIGHTKG" & slog,]
-  
-  subj_keep <- rep("Include", nrow(w_df))
-  names(subj_keep) <- w_df$id
-  
-  subj_df <- w_df
-  
-  # 1w ----
-  # 1w. remove biologically impossible weight records
-  criteria <- remove_biv(subj_df, "weight", biv_df)
-  subj_keep[criteria] <- "Implausible"
-  
-  subj_df <- subj_df[!criteria,]
-  
-  if (nrow(subj_df) > 0){
-    # 2w ----
-    # 2w. weight inaccurate if:
-    # a) the range was > 22.7 kg AND absolute difference between recorded weight 
-    # and avg weight was > 70% of range
-    # OR
-    # b) SD was >20% of the average weight AND absolute difference between that 
-    # weight and average weight > the SD
-    
-    avg_w <- mean(subj_df$measurement)
-    
-    # first calculate criteria a)
-    w_range <- abs(max(subj_df$measurement) - min(subj_df$measurement))
-    # if range is > 22.7, we can possibly say something about weight
-    criteria_a <- 
-      if (w_range > 22.7){
-        # weight difference is more than 70% of range
-        sapply(subj_df$measurement, function(x){
-          abs(x - avg_w) > .7*w_range
-        })
-      } else {
-        rep(F, nrow(subj_df))
-      }
-    
-    # criteria b)
-    st_dev <- sd(subj_df$measurement)
-    # if sd was >20% of avg weight, we can possibly say something about weight
-    criteria_b <- 
-      if (st_dev/avg_w > .2){
-        # weight difference is > SD
-        sapply(subj_df$measurement, function(x){
-          abs(x - avg_w) > st_dev
-        })
-      } else {
-        rep(F, nrow(subj_df))
-      }
-    
-    subj_keep[as.character(subj_df$id[criteria_a | criteria_b])] <- "Implausible"
-  }
-  
-  w_df$result <- subj_keep
-  
-  # add results to full dataframe
-  df[as.character(w_df$id), "result"] <- w_df$result
+  return(df)
 }
