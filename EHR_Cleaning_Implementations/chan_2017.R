7f461489e7ee39ca3397ac6fc2b6e52f8de6b12b
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-05 11:15:49 -0400

    Functionalize for future shiny app

--- EHR_Cleaning_Implementations/chan_2017.R
+++ EHR_Cleaning_Implementations/chan_2017.R
@@ -5,6 +5,7 @@
 # paper: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5359164/
 
 # Note: will have to update to data table upon completion for speed
+# Note 2: must remove missing values before running method
 
 # supporting functions ----
 
@@ -24,113 +25,128 @@ remove_biv <- function(subj_df, type, biv_df, include = F){
 
 # implement chan, et al. ----
 
-# method specific constants ----
-# this includes specified cutoffs, etc.
+# function to clean height and weight data by chan, et al.
+# inputs:
+# df: data frame with 7 columns:
+#   id: row id
+#   subjid: subject id
+#   sex: sex of subject
+#   age_years: age, in years
+#   param: HEIGHTCM or WEIGHTKG
+#   measurement: height or weight measurement
+# outputs:
+#   df, with additional column "result", which specifies whether the 
+#     measurement should be included or implausible.
+chan_clean_both <- function(df){
+  # method specific constants ----
+  # this includes specified cutoffs, etc.
+  
+  # BIVs
+  biv_df <- data.frame(
+    "low" = c(48*2.54, 22.7, 10),
+    "high" = c(84*2.54, 340.2, 100)
+  )
+  rownames(biv_df) <- c("height", "weight", "bmi")
+  
+  # begin implementation ----
 
-# BIVs
-biv_df <- data.frame(
-  "low" = c(48*2.54, 22.7, 10),
-  "high" = c(84*2.54, 340.2, 100)
-)
-rownames(biv_df) <- c("height", "weight", "bmi")
-
-# begin implementation ----
-
-
-# preallocate final designation
-df$result <- "Include"
-rownames(df) <- df$id
-# go through each subject
-for (i in unique(df$subjid)){
-  slog <- df$subjid == i
-  
-  # start with height ----
-  h_df <- df[df$param == "HEIGHTCM" & slog,]
-  
-  subj_keep <- rep("Include", nrow(h_df))
-  names(subj_keep) <- h_df$id
-  
-  subj_df <- h_df
-  
-  # 1h ----
-  # 1h. remove biologically impossible height records
-  criteria <- remove_biv(subj_df, "height", biv_df)
-  subj_keep[criteria] <- "Implausible"
-  
-  subj_df <- subj_df[!criteria,]
-  
-  # 2h ----
-  # 2h. Exclude heights that were greater than 3 standard deviations from the 
-  # mean.
-  
-  if (nrow(subj_df) > 0){
-    # calculate mean and standard deviation
-    avg_ht <- mean(subj_df$measurement)
-    st_dev_ht <- sd(subj_df$measurement)
+  # preallocate final designation
+  df$result <- "Include"
+  rownames(df) <- df$id
+  # go through each subject
+  for (i in unique(df$subjid)){
+    slog <- df$subjid == i
     
-    # calculate exclusion criteria
-    criteria <- abs(subj_df$measurement - avg_ht) > 3*st_dev_ht
+    # start with height ----
+    h_df <- df[df$param == "HEIGHTCM" & slog,]
     
-    subj_keep[as.character(subj_df$id)][criteria] <- "Implausible"
-  }
-  
-  # add the full calculation
-  h_df$result <- subj_keep
-  
-  # add results to full dataframe
-  df[as.character(h_df$id), "result"] <- h_df$result
-  
-  # move to weight ----
-  
-  w_df <- df[df$param == "WEIGHTKG" & slog,]
-  
-  subj_keep <- rep("Include", nrow(w_df))
-  names(subj_keep) <- w_df$id
-  
-  subj_df <- w_df
-  
-  # 1w ----
-  # 1w. remove biologically impossible weight records
-  criteria <- remove_biv(subj_df, "weight", biv_df)
-  subj_keep[criteria] <- "Implausible"
-  
-  subj_df <- subj_df[!criteria,]
-  
-  # 2w ----
-  # 2w. Calculate BMI based on average height, then remove BMI bivs.
-  
-  if (sum(h_df$result == "Include") > 0 & nrow(subj_df) > 0){
-    # calculate avg height for subject
-    avg_ht <- mean(h_df$measurement[h_df$result == "Include"])
+    subj_keep <- rep("Include", nrow(h_df))
+    names(subj_keep) <- h_df$id
     
-    # calulate bmi
-    bmi_df <- data.frame(
-      "measurement" = w_df$measurement/((avg_ht/100)^2)
-    )
+    subj_df <- h_df
     
-    criteria <- remove_biv(bmi_df, "bmi", biv_df, include = T)
-    subj_keep[as.character(w_df$id[criteria])] <- "Implausible"
+    # 1h ----
+    # 1h. remove biologically impossible height records
+    criteria <- remove_biv(subj_df, "height", biv_df)
+    subj_keep[criteria] <- "Implausible"
     
     subj_df <- subj_df[!criteria,]
+    
+    # 2h ----
+    # 2h. Exclude heights that were greater than 3 standard deviations from the 
+    # mean.
+    
+    if (nrow(subj_df) > 0){
+      # calculate mean and standard deviation
+      avg_ht <- mean(subj_df$measurement)
+      st_dev_ht <- sd(subj_df$measurement)
+      
+      # calculate exclusion criteria
+      criteria <- abs(subj_df$measurement - avg_ht) > 3*st_dev_ht
+      
+      subj_keep[as.character(subj_df$id)][criteria] <- "Implausible"
+    }
+    
+    # add the full calculation
+    h_df$result <- subj_keep
+    
+    # add results to full dataframe
+    df[as.character(h_df$id), "result"] <- h_df$result
+    
+    # move to weight ----
+    
+    w_df <- df[df$param == "WEIGHTKG" & slog,]
+    
+    subj_keep <- rep("Include", nrow(w_df))
+    names(subj_keep) <- w_df$id
+    
+    subj_df <- w_df
+    
+    # 1w ----
+    # 1w. remove biologically impossible weight records
+    criteria <- remove_biv(subj_df, "weight", biv_df)
+    subj_keep[criteria] <- "Implausible"
+    
+    subj_df <- subj_df[!criteria,]
+    
+    # 2w ----
+    # 2w. Calculate BMI based on average height, then remove BMI bivs.
+    
+    if (sum(h_df$result == "Include") > 0 & nrow(subj_df) > 0){
+      # calculate avg height for subject
+      avg_ht <- mean(h_df$measurement[h_df$result == "Include"])
+      
+      # calulate bmi
+      bmi_df <- data.frame(
+        "measurement" = w_df$measurement/((avg_ht/100)^2)
+      )
+      
+      criteria <- remove_biv(bmi_df, "bmi", biv_df, include = T)
+      subj_keep[as.character(w_df$id[criteria])] <- "Implausible"
+      
+      subj_df <- subj_df[!criteria,]
+    }
+    
+    # 3w ----
+    # 3w. Exclude weights that were greater than 3 standard deviations from the mean.
+    
+    if (nrow(subj_df) > 0){
+      # calculate mean and standard deviation
+      avg_wt <- mean(subj_df$measurement)
+      st_dev_wt <- sd(subj_df$measurement)
+      
+      # calculate exclusion criteria
+      criteria <- abs(subj_df$measurement - avg_wt) > 3*st_dev_wt
+      
+      subj_keep[as.character(subj_df$id)][criteria] <- "Implausible"
+    }
+    
+    # add the full calculation
+    w_df$result <- subj_keep
+    
+    # add results to full dataframe
+    df[as.character(w_df$id), "result"] <- w_df$result
   }
   
-  # 3w ----
-  # 3w. Exclude weights that were greater than 3 standard deviations from the mean.
-  
-  if (nrow(subj_df) > 0){
-    # calculate mean and standard deviation
-    avg_wt <- mean(subj_df$measurement)
-    st_dev_wt <- sd(subj_df$measurement)
-    
-    # calculate exclusion criteria
-    criteria <- abs(subj_df$measurement - avg_wt) > 3*st_dev_wt
-    
-    subj_keep[as.character(subj_df$id)][criteria] <- "Implausible"
-  }
-  
-  # add the full calculation
-  w_df$result <- subj_keep
-  
-  # add results to full dataframe
-  df[as.character(w_df$id), "result"] <- w_df$result
+  return(df)
 }
