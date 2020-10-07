52ff5114e41e383811784cc8df5299c085ba85ba
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-07 16:04:50 -0400

    Add libraries and data

---
+++ adult_cleaning_app.R
@@ -0,0 +1,21 @@
+# Adult growthcleanr Explorer
+# By Hannah De los Santos
+# Originated on: 10/7/2020
+
+# This implements a prototype application to explore adult EHR cleaning 
+# implementations.
+
+# load libraries, scripts, and data ----
+
+library(shiny)
+library(ggplot2)
+library(rstudioapi)
+
+#https://stackoverflow.com/questions/3452086/getting-path-of-an-r-script/35842176#35842176
+# set working directory - only works in RStudio (with rstudioapi)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# load data - fake for now, will get a better
+df <- read.csv(file.path("Data", "adult_synthetic_data_seed_8.csv"))
+
+# load requisite functions
