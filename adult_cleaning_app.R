122b177ed13321c04cf31c7101fa59b9f93c4236
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-08 09:11:05 -0400

    Add function implementations

--- adult_cleaning_app.R
+++ adult_cleaning_app.R
@@ -19,3 +19,13 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 df <- read.csv(file.path("Data", "adult_synthetic_data_seed_8.csv"))
 
 # load requisite functions
+# function below from ?source
+sourceDir <- function(path, trace = TRUE, ...) {
+  for (nm in list.files(path, pattern = "[.][RrSsQq]$")) {
+    # if(trace) cat(nm,":")
+    source(file.path(path, nm), ...)
+    # if(trace) cat("\n")
+  }
+}
+
+sourceDir("EHR_Cleaning_Implementations")
