b6fce937650d960151f70b7fc8615facad9be4d4
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2020-10-28 11:56:54 -0400

    Add breland preamble

---
+++ EHR_Cleaning_Implementations/breland_2017.R
@@ -0,0 +1,8 @@
+# Implementing Breland, et al. (2017)
+# By Hannah De los Santos
+# Originated on: 10/28/2020
+
+# paper: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5359156/
+
+# implement breland, et al. ----
+
