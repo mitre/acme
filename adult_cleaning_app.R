a06a9e22074351b3eaa62947ac50a08025d6ab0a
Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2021-02-01 12:33:02 -0500

    Update version

--- adult_cleaning_app.R
+++ adult_cleaning_app.R
@@ -5,7 +5,7 @@
 # This implements a prototype application to explore adult EHR cleaning 
 # implementations.
 
-vers_adult_ehr <- "0.3.7"
+vers_adult_ehr <- "1.0.0"
 
 # load libraries, scripts, and data ----
 
