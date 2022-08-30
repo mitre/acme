c97cbf037d15f19013189bcff2ea2b205e0ee77e
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2022-08-30 15:48:36 -0400

    Implement shi algorithm in explorer format

--- EHR_Cleaning_Implementations/infants/shi_2018.R
+++ EHR_Cleaning_Implementations/infants/shi_2018.R
@@ -12,210 +12,253 @@
 # @author = Max Olivier
 ################################################################################
 
-# Import necessary libraries
-library(data.table)
+# function to clean weight data by shi, et al.
+# inputs:
+# df: data frame with 7 columns:
+#   id: row id, must be unique
+#   subjid: subject id
+#   sex: sex of subject
+#   age_days: age, in days
+#   param: HEIGHTCM or WEIGHTKG
+#   measurement: height or weight measurement
+# inter_vals: boolean, return intermediate values
+# outputs:
+#   df, with additional columns:
+#     result, which specifies whether the height measurement should be included,
+#       or is implausible.
+#     reason, which specifies, for implausible values, the reason for exclusion,
+#       and the step at which exclusion occurred.
+#     intermediate value columns, if specified
+shi_clean_both <- function(df, inter_vals = F){
 
-################################################################################
-# Import and clean data for testing----
-# Algorithm needs 4 columns of data: patient/subject ID, age (in days), actual
-# measurement, and for-age-z-score of measurement that are created from multiple
-# sources (outlined on pg. 211.e1 of paper). Data should be in long format.
-################################################################################
+  # method specific constants ----
 
-# Import synthetic data set for testing.
+  inter_cols <- c(
+    "Step_1h_WHO_Z_for_Age",
+    "Step_1h_H_BIV_Low_Compare",
+    "Step_1h_H_BIV_High_Compare",
+    "Step_1h_Result",
+    "Step_1w_WHO_Z_for_Age",
+    "Step_1w_W_BIV_Low_Compare",
+    "Step_1w_W_BIV_High_Compare",
+    "Step_1w_Result",
+    "Step_2h_First_Age_Diff_and_Criteria",
+    "Step_2h_First_SD_Diff_and_Criteria",
+    "Step_2h_WHO_Z_for_Age",
+    "Step_2h_Subject_SD",
+    "Step_2h_Result",
+    "Step_2w_First_Age_Diff_and_Criteria",
+    "Step_2w_First_SD_Diff_and_Criteria",
+    "Step_2w_WHO_Z_for_Age",
+    "Step_2w_Subject_SD",
+    "Step_2w_Result"
+  )
 
-dat <- fread("C:/Users/molivier/Documents/Projects/CDC_CODI/Data_sets/gc-observations.csv")
+  # begin implementation ----
 
-# Filter out the data in several ways. Just keep weight data (since original
-# algorithm uses weight data) and drop all the columns
-df <- dat[, c("subjid", "param", "measurement", "sex",  "age_days")]
+  # preallocate final designation
+  df$result <- "Include"
+  df$reason <- ""
+  df$id <- as.character(df$id)
+  rownames(df) <- df$id
 
-# UPLOAD DATA SETS AS WELL
+  # if using intermediate values, we want to start storing them
+  # keep the ID to collate with the final dataframe
+  inter_df <- df[, "id", drop = F]
+  rownames(inter_df) <- inter_df$id
 
-################################################################################
-# Shi algorithm, part (2)----
-# Setting up variables and lists of ID's to loop over.
-################################################################################
+  # in order not to add additional columns to output (unless called for),
+  # we create a dataframe to use for computation
+  all_df <- df
 
-# Create standardized measurement. Assumes function is already loaded.
-df[, stdz_meas := get_standardized_infant_scores(param, age_days, sex, measurement)]
-
-# Sort data by subject, param, and age.
-df <- df[order(df$subjid, df$param, df$age_days),]
-
-df <- df[1:100000,]
-
-# Generate variables to store jackknife residuals. Make sure they are numerics
-# so that we can add the values in later.
-df[,raw_resid := as.numeric(NA)]
-df[,stdz_resid := as.numeric(NA)]
-
-# Get the number of non-null observations per subject for both standardized and
-# raw measurements for each of height and weight.
-ht_nobs_id_stdz <- table(df[!is.na(df$stdz_meas) &
-                            df$param=="HEIGHTCM",]$subjid)
-ht_nobs_id_raw <- table(df[!is.na(df$measurement) &
-                            df$param=="HEIGHTCM",]$subjid)
-wt_nobs_id_stdz <- table(df[!is.na(df$stdz_meas) &
-                            df$param=="WEIGHTKG",]$subjid)
-wt_nobs_id_raw <- table(df[!is.na(df$measurement) &
-                            df$param=="WEIGHTKG",]$subjid)
-
-
-################################################################################
-# Shi algorithm, part (3)----
-# Getting jackknife residuals for standardized measurements. We do this for both
-# height and weight while they just do the height version in the paper.
-################################################################################
-
-# Pick out the ids with more than three observations. More than three are needed
-# to be able to compute the jackknife residuals since there are two regression
-# coefficients and we need to run the "one left out" regressions.
-ht_reg_ids_stdz <- names(ht_nobs_id_stdz[ht_nobs_id_stdz > 3])
-
-for(x in ht_reg_ids_stdz) {
-  subset_obs <- df$subjid==x & df$param=="HEIGHTCM" & !is.na(df$stdz_meas)
-  reg <- lm(stdz_meas ~ age_days, data=df[subset_obs,])
-  jk_res <- rstudent(reg) # Built in R function for jackknife residuals
-  df[subset_obs, "stdz_resid"] <- jk_res
-}
-
-# Same regression, but for weight observations.
-wt_reg_ids_stdz <- names(wt_nobs_id_stdz[wt_nobs_id_stdz > 3])
-
-for(x in wt_reg_ids_stdz) {
-  subset_obs <- df$subjid==x & df$param=="WEIGHTKG" & !is.na(df$stdz_meas)
-  reg <- lm(stdz_meas ~ age_days, data=df[subset_obs,])
-  jk_res <- rstudent(reg) # Built in R function for jackknife residuals
-  df[subset_obs, "stdz_resid"] <- jk_res
-}
-
-################################################################################
-# Shi algorithm, part (4)----
-# Getting jackknife residuals for raw measurements. Do this for both height and
-# weight.
-################################################################################
-
-# For the same reasons as above, pick out the ids with more than three
-# observations.
-ht_reg_ids_raw <- names(ht_nobs_id_raw[ht_nobs_id_raw > 3])
-
-# Here the regression in on square root of age instead of age, per the authors'
-# work.
-for(x in ht_reg_ids_raw) {
-  subset_obs <- df$subjid==x & df$param=="HEIGHTCM" & !is.na(df$measurement)
-  reg <- lm(measurement ~ I(sqrt(age_days)), data=df[subset_obs,])
-  jk_res <- rstudent(reg) # Built in R function for jackknife residuals
-  df[subset_obs, "raw_resid"] <- jk_res
-}
-
-# Repeat for weights.
-wt_reg_ids_raw <- names(wt_nobs_id_raw[wt_nobs_id_raw > 3])
-
-for(x in wt_reg_ids_raw) {
-  subset_obs <- df$subjid==x & df$param=="WEIGHTKG" & !is.na(df$measurement)
-  reg <- lm(measurement ~ I(sqrt(age_days)), data=df[subset_obs,])
-  jk_res <- rstudent(reg) # Built in R function for jackknife residuals
-  df[subset_obs, "raw_resid"] <- jk_res
-}
-
-
-################################################################################
-# Shi algorithm, part (5)----
-# Identify potential outliers as those whose jackknife residual is more than
-# four in absolute value.
-################################################################################
-
-# The cases where the outlier value is NA correspond to cases where the residual
-# was NA, meaning the regression wasn't run for this ID since it didn't have
-# enough observations. Per the algorithm, mark these with a 9 to indicate
-# "should be further investigated"
-
-df$stdz_outlier <- abs(df$stdz_resid) > 4
-df$stdz_outlier[is.na(df$stdz_outlier)] <- 9
-
-df$raw_outlier <- abs(df$raw_resid) > 4
-df$raw_outlier[is.na(df$raw_outlier)] <- 9
-
-################################################################################
-# Shi algorithm, part (6)----
-# Identify biologically implausible values. These are identified by looking for
-# biologically implausible pairs, which are adjacent height observations where
-# the raw measure fell, or adjacent weight observations where the raw measure
-# fell by more than 15%.
-################################################################################
-
-# Create a columns that will indicate if values are biologically implausible.
-df[,raw_biv := as.numeric(NA)]
-df[,stdz_biv := as.numeric(NA)]
-
-# Go through all of the ID's to find biologically implausible pairs, and label
-# one value in the pair as a biologically implausible value. To determine which
-# of the two is the implausible value, look at the residuals, and the one with
-# the larger absolute value residual is considered the implausible value. This
-# process is repeated for both standardized and raw residuals. Also address both
-# height and weight together in the same loop for each ID.
-for(id in unique(df$subjid)) {
-  # Find all the height observations for this ID.
-  ht_idvar_locs <- which(df$subjid==id & df$param=="HEIGHTCM")
-
-  # Only go through checking if there is
-  if(length(ht_idvar_locs) > 1) {
-    for(i in 1:(length(ht_idvar_locs)-1)) {
-      # These two variables are just for readability later on.
-      loc1 <- ht_idvar_locs[i]
-      loc2 <- ht_idvar_locs[i+1]
-      # Actually identifying if this pair is biologically implausible. Need to
-      # make sure that there are non-null residuals to compare as well as
-      # checking the value decrease.
-      if(!is.na(df$raw_resid[loc1]) & !is.na(df$raw_resid[loc2]) &
-         df$measurement[loc1] > df$measurement[loc2]) {
-        # And find the residual with the larger absolute value.
-        if(abs(df$raw_resid[loc1]) > abs(df$raw_resid[loc2])) {
-          df[loc1, "raw_biv"] <- 1
-        }
-        else {
-          df[loc2, "raw_biv"] <- 1
-        }
-      }
-      # Repeat for the standardized residuals.
-      if(!is.na(df$stdz_resid[loc1]) & !is.na(df$stdz_resid[loc2]) &
-         df$measurement[loc1] > df$measurement[loc2]) {
-        if(abs(df$stdz_resid[loc1]) > abs(df$stdz_resid[loc2])) {
-          df[loc1, "stdz_biv"] <- 1
-        }
-        else {
-          df[loc2, "stdz_biv"] <- 1
-        }
-      }
-    }
+  if (inter_vals){
+    # if using intermediate values, preallocate values
+    df[, inter_cols] <- NA
+    inter_df[, inter_cols] <- NA
   }
 
-  # Repeat for weights.
-  wt_idvar_locs <- which(df$subjid==id & df$param=="WEIGHTKG")
+  # preprocessing ----
 
-  if(length(wt_idvar_locs) > 1) {
-    for(i in 1:(length(wt_idvar_locs)-1)) {
-      loc1 <- wt_idvar_locs[i]
-      loc2 <- wt_idvar_locs[i+1]
-      if(!is.na(df$raw_resid[loc1]) & !is.na(df$raw_resid[loc2]) &
-         df$measurement[loc2] < .85*df$measurement[loc1]) {
-        if(abs(df$raw_resid[loc1]) > abs(df$raw_resid[loc2])) {
-          df[loc1, "raw_biv"] <- 1
+  # Create standardized measurement. Assumes function is already loaded.
+  all_df$stdz_meas <- get_standardized_infant_scores(
+    all_df$param, all_df$age_days, all_df$sex, all_df$measurement
+  )
+
+  # Sort data by subject, param, and age.
+  all_df <- all_df[order(all_df$subjid, all_df$param, all_df$age_days),]
+
+  # Generate variables label outliers and "invalid inliers".
+  all_df$raw_resid <- all_df$stdz_resid <- F
+  
+  # Create a columns that will indicate if values are biologically implausible.
+  all_df$biv <- F
+  
+  # start going through the main algorithm ----
+  
+  for (type in c("HEIGHTCM", "WEIGHTKG")){
+    step_base <- tolower(substr(type, 1, 1))
+    
+    
+    # Get the number of non-null observations per subject for both standardized and
+    # raw measurements for each of height and weight.
+    nobs_id_stdz <- table(all_df[!is.na(all_df$stdz_meas) &
+                              all_df$param==type, "subjid"])
+    nobs_id_raw <- table(all_df[!is.na(all_df$measurement) &
+                             all_df$param==type,"subjid"])
+    
+    
+    # 1h/w, H/W jackknife comparison ----
+    # 1h/w: get jaccknife residuals for both standardized and raw measurements. 
+    # If the residual > 4, mark as outlier. If it doesn't exist, mark as "should
+    # be further investigated."
+    step <- paste0("1", step_base, ", ", toupper(step_base),
+                   " jackknife comparison")
+    
+    # standardized first:
+    
+    # Pick out the ids with more than three observations. More than three are needed
+    # to be able to compute the jackknife residuals since there are two regression
+    # coefficients and we need to run the "one left out" regressions.
+    reg_ids_stdz <- names(nobs_id_stdz[nobs_id_stdz > 3])
+    
+    
+    # MAYBE TODO: SAPPLY?
+    for(x in reg_ids_stdz) {
+      subset_obs <- all_df$subjid==x & all_df$param==type & 
+        !is.na(all_df$stdz_meas)
+      reg <- lm(stdz_meas ~ age_days, data = all_df[subset_obs,])
+      jk_res <- rstudent(reg) # Built in R function for jackknife residuals
+      all_df[subset_obs, "stdz_resid"] <- jk_res
+    }
+    
+    # raw second:
+    # Getting jackknife residuals for raw measurements. Do this for both height and
+    # weight.
+    
+    # For the same reasons as above, pick out the ids with more than three
+    # observations.
+    reg_ids_raw <- names(nobs_id_raw[nobs_id_raw > 3])
+    
+    # Here the regression in on square root of age instead of age, per the authors'
+    # work.
+    for(x in reg_ids_raw) {
+      subset_obs <- all_df$subjid==x & all_df$param==type & 
+        !is.na(all_df$measurement)
+      reg <- lm(measurement ~ I(sqrt(age_days)), data=all_df[subset_obs,])
+      jk_res <- rstudent(reg) # Built in R function for jackknife residuals
+      all_df[subset_obs, "raw_resid"] <- jk_res
+    }
+    
+    # outlier identification 
+    # Identify potential outliers as those whose jackknife residual is more than
+    # four in absolute value.
+    
+    # The cases where the outlier value is NA correspond to cases where the residual
+    # was NA, meaning the regression wasn't run for this ID since it didn't have
+    # enough observations. Per the algorithm, mark these with a 9 to indicate
+    # "should be further investigated"
+    
+    # assign to output
+    all_df[(is.na(all_df$stdz_resid) | is.na(all_df$raw_resid)), 
+           "reason"] <- 
+      paste0("No Jackknife Investigate Further, Step ", step)
+    
+    all_df$stdz_outlier <- abs(all_df$stdz_resid) > 4 & !is.na(all_df$stdz_resid)
+    
+    all_df$raw_outlier <- abs(all_df$raw_resid) > 4 & !is.na(all_df$raw_resid)
+    
+    # assign to output
+    all_df[all_df$stdz_outlier | all_df$raw_outlier, "result"] <- "Implausible"
+    all_df[all_df$stdz_outlier & !all_df$raw_outlier, "reason"] <- 
+      paste0("Standarized Outlier, Step ", step)
+    all_df[!all_df$stdz_outlier & all_df$raw_outlier, "reason"] <- 
+      paste0("Raw Outlier, Step ", step)
+    all_df[all_df$stdz_outlier & all_df$raw_outlier, "reason"] <- 
+      paste0("Both Outlier, Step ", step)
+    
+    
+    # 2h/w, H/W BIV ----
+    # 2h/w, Identify biologically implausible in sequential pairs. If measurement
+    # of earlier time point is > next time point (for height) or measurement of
+    # earlier time point*0.85 > next time point (for weight), identify larger
+    # absolute residual as BIV, for both standardized and raw residuals.
+    step <- paste0("2", step_base, ", ", toupper(step_base),
+                   " BIV")
+    
+    # These are identified by looking for
+    # biologically implausible pairs, which are adjacent height observations where
+    # the raw measure fell, or adjacent weight observations where the raw measure
+    # fell by more than 15%.
+    
+    
+    # ONLY CHECK WHERE ALL THE THE SUBJECT ISN'T ALL OUTLIERS
+    
+    # Go through all of the ID's to find biologically implausible pairs, and label
+    # one value in the pair as a biologically implausible value. To determine which
+    # of the two is the implausible value, look at the residuals, and the one with
+    # the larger absolute value residual is considered the implausible value. This
+    # process is repeated for both standardized and raw residuals. Also address both
+    # height and weight together in the same loop for each ID.
+    for (id in unique(all_df$subjid)) {
+      # Find all the height observations for this ID.
+      idvar_locs <- which(all_df$subjid==id & all_df$param==type)
+      
+      subj_df <- all_df[idvar_locs,]
+      
+      # Only go through checking if there is enough
+      if(nrow(subj_df) > 1) {
+        # make the column we're comparing to and the lagged-- next value column
+        subj_df$raw_resid_next <- c(subj_df$raw_resid[-1], NA)
+        subj_df$stdz_resid_next <- c(subj_df$stdz_resid[-1], NA)
+        subj_df$measurement_next <- c(subj_df$measurement[-1], NA)
+        
+        for (check_val in c("raw_resid", "stdz_resid")){
+          
+          # Actually identifying if this pair is biologically implausible. Need to
+          # make sure that there are non-null residuals to compare as well as
+          # checking the value decrease.
+          criteria <- !is.na(subj_df[, check_val]) & 
+            !is.na(subj_df[, paste0(check_val, "_next")]) &
+            if (type == "HEIGHTCM"){
+              subj_df$measurement > subj_df$measurement_next 
+            } else { # weight
+              subj_df$measurement*.85 > subj_df$measurement_next 
+            }
+          
+          # first -- check that they both exist
+          # then -- check that first measurement is bigger than the next
+          subj_df[
+            criteria & 
+              abs(subj_df[, check_val]) > 
+              abs(subj_df[, paste0(check_val, "_next")]),
+            "biv"
+          ] <- T
+          
+          subj_df[
+            criteria & 
+              abs(subj_df[, check_val]) <= 
+              abs(subj_df[, paste0(check_val, "_next")]),
+            "biv"
+          ] <- T
         }
-        else {
-          df[loc2, "raw_biv"] <- 1
-        }
-      }
-      if(!is.na(df$stdz_resid[loc1]) & !is.na(df$stdz_resid[loc2]) &
-           df$measurement[loc2] < .85*df$measurement[loc1]) {
-        if(abs(df$stdz_resid[loc1]) > abs(df$stdz_resid[loc2])) {
-          df[loc1, "stdz_biv"] <- 1
-        }
-        else {
-          df[loc2, "stdz_biv"] <- 1
-        }
+        
+        all_df[subj_df$id, "biv"] <- subj_df$biv
       }
     }
+    
+    # all df has the output
+    
+    # assign to output
+    all_df[all_df$biv, "result"] <- "Implausible"
+    all_df[all_df$biv, "reason"] <- 
+      paste0("Implausible, Step ", step)
+    
   }
+  
+  # add results to full dataframe
+  df[all_df$id, "result"] <- all_df$result
+  df[all_df$id, "reason"] <- all_df$reason
+  
+  return(df)
 }
+
